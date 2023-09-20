3ad428c090ba5f694608f2c898315478cedc44e3
Ian Thuku <thuku0249@gmail.com>
2023-09-20 16:02:37 -0700

    Shell command line

---
+++ main.c
@@ -0,0 +1,35 @@
+#include "shell.h"
+/**
+ * main - start point
+ * @argc: argument count
+ * @argv: arguments
+ * Return: 0 for success
+ */
+int main(int argc, char **argv)
+{
+	char *s = NULL;
+	char **command = NULL;
+	int status = 0;
+
+	(void) argc;
+	while (1)
+	{
+		s = scan_line();
+		if (s  == NULL)
+		{
+			if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "n", 1);
+			return (status);
+		}
+		command = tokenize(s);
+		if (!command)
+		{
+			free(line);
+			continue;
+		}
+		status = _execute(command, argv);
+		free(s);
+		free_command(command);
+	}
+	return (status);
+}
